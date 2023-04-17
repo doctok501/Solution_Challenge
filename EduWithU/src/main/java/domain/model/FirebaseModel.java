655339787fb915712a84ef7767f60837d660b1ca
kim_ingyeom <doctok501@gmail.com>
2023-04-17 19:22:46 +0900

    Solution_Challenge

---
+++ EduWithU/src/main/java/domain/model/FirebaseModel.java
@@ -0,0 +1,22 @@
+package domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FirebaseModel {
+
+    @Id
+    @GeneratedValue
+    @Column
+    private String id;
+
+
+}
